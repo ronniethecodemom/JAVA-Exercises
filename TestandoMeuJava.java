b5c7516d9401bf8e344a519bf7226fb8d61ae469
Ronnie (Verônica) <veronicamarquesfranco@gmail.com>
2024-10-29 14:14:54 -0300

    Add files via upload

---
+++ TestandoMeuJava.java
@@ -0,0 +1,16 @@
+class TestandoMeuJava {
+
+	public static void main (String[]args){
+
+		String name = "Verônica";
+		int age = 30;
+		char gender = 'F';
+		double height = 1.59;
+		
+		System.out.println ("Hello, world! This is my first code test in Java! Iuhuuu!");
+
+		System.out.println ("My name is " + name + ", I'm " + age + " years old, my gender is " + gender + " and my height is "+ height+".");
+		
+	System.exit(0);
+	}
+}
